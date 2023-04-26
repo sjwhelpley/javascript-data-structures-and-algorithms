e5754de49717053be425d8bdb41edc3719d80b5e
Samantha Whelpley <35855872+sjwhelpley@users.noreply.github.com>
2023-04-26 13:49:33 -0400

    Add files via upload

---
+++ palindrome-checker.js
@@ -0,0 +1,10 @@
+function palindrome(str) {
+  const formatted = str.replace(/[^a-z0-9]/gi, "").toLowerCase()
+  console.log(formatted)
+  for(var i = 0, len = formatted.length - 1; i < len/2; i++) {
+    if (formatted[i] !== formatted[len - i]) {
+      return false
+    }
+  }
+  return true;
+}
